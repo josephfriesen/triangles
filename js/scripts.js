6c3460f3090ad1754d85675be51fb2570ecccb27
Joseph Friesen <friesen.josephc@gmail.com>
2018-08-09 15:35:31 -0700

    Add JavaScript function to find the angles of a given triangle and jQuery function to place the answers in the output on the main page. Also add function to convert angles given by function findAngle as radians to degrees with function radToDeg.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 /* Business-side logic: functions to calculate angle measurments given triangle side lengths */
 
+/* A function to square a number, probably not necessary but here it is anyway. */
 var sqr = function(x) {
   square = x * x;
   return square;
 };
 
 /* Calculate the angle (in radians) of a triangle given the lengths of the three sides, where a and b are the lengths of the sides that intersect in the angle to be found, and c is the length of the opposite side. Ex: given the right triangle ABC, where side A is length 3, side B is length 4, and side C, the hypotenuse, is length 5, angleAB(3,4,5) = pi/2. */
-var angleAB = function(a,b,c) {
+var findAngle = function(a,b,c) {
   numerator = sqr(a) + sqr(b) - sqr(c);
   denom = 2*a*b;
   arg = numerator / denom;
@@ -14,7 +15,11 @@ var angleAB = function(a,b,c) {
   return result;
 };
 
-
+/* Convert an angle given in radians to degrees. */
+var radToDeg = function(x) {
+  var pi = Math.PI;
+  return x * (180/pi);
+}
 
 
 
@@ -47,9 +52,20 @@ $(document).ready(function() {
       triangleType = "not a triangle";
     };
 
+    if (triangleType != "not a triangle") {
+      var angleAB = radToDeg(findAngle(side1, side2, side3)).toFixed(3);
+      var angleBC = radToDeg(findAngle(side2, side3, side1)).toFixed(3);
+      var angleAC = radToDeg(findAngle(side1, side3, side2)).toFixed(3);
+      console.log(angleAB, angleBC, angleAC);
+    };
+
     $("#side-A-len").text(side1);
     $("#side-B-len").text(side2);
     $("#side-C-len").text(side3);
+
+    $("#angle-AB").text(angleAB);
+    $("#angle-BC").text(angleBC);
+    $("#angle-AC").text(angleAC);
 
     if (triangleType === "not a triangle") {
       $(".not-a-triangle").show();
